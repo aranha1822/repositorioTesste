382c68a137df1d4306e698c6f64964ffa4f47ded
CAUA HENRIQUE SOUZA COSTA <caua.3932@aluno.pr.senac.br>
2023-09-05 16:26:16 -0300

    primeira alteracao

--- src/exemplo_repositorio/Exemplo_repositorio.java
+++ src/exemplo_repositorio/Exemplo_repositorio.java
@@ -15,6 +15,8 @@ public class Exemplo_repositorio {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Esta alterado");
     }
     
 }
