b5ecafa235726066db7b1815eaa90a7878b783d6
CAUA HENRIQUE SOUZA COSTA <caua.3932@aluno.pr.senac.br>
2023-09-05 15:45:17 -0300

    primeiro commit

---
+++ src/exemplo_repositorio/Exemplo_repositorio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exemplo_repositorio;
+
+/**
+ *
+ * @author caua.3932
+ */
+public class Exemplo_repositorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
